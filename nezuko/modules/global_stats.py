17186570ba7a5a2982a08c6555f7488d7024fdae
definitelynotchirag <chiragdave1888@gmail.com>
2023-03-06 23:21:05 +0530

    Update global_stats.py

--- nezuko/modules/global_stats.py
+++ nezuko/modules/global_stats.py
@@ -103,11 +103,11 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://github.com/definitelynotchirag/DomaBot/contributors"
+#     url = "https://github.com/definitelynotchirag/DomaBot/contributors"
     rurl = "https://github.com/definitelynotchirag/DomaBot"
-    developers = await get(url)
-    commits = sum(developer["contributions"] for developer in developers)
-    developers = len(developers)
+#     developers = await get(url)
+#     commits = sum(developer["contributions"] for developer in developers)
+#     developers = len(developers)
 
     # Rss feeds
     rss_count = await get_rss_feeds_count()
@@ -129,7 +129,6 @@ async def global_stats(_, message):
     **{warns_count}** Warns, Across **{warns_chats_count}** chats.
     **{karmas_count}** Karma, Across **{karmas_chats_count}** chats.
     **{served_users}** Users, Across **{served_chats}** chats.
-    **{developers}** Developers And **{commits}** Commits On **[Github]({rurl})**.
 
 """
     await m.edit(msg, disable_web_page_preview=True)
